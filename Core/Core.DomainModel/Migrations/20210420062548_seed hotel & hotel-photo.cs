6e8553970d4e4c82a6128a1032581be683d361d9
Sarah <Sarah_Bani@outlook.com>
2021-05-09 17:03:41 +0430

    some unimportant corrections

--- Core/Core.DomainModel/Migrations/20210420062548_seed hotel & hotel-photo.cs
+++ Core/Core.DomainModel/Migrations/20210420062548_seed hotel & hotel-photo.cs
@@ -7,6 +7,7 @@ namespace Core.DomainModel.Migrations
         protected override void Up(MigrationBuilder migrationBuilder)
         { 
             string sql = @"
+INSERT INTO hotel (id, name, city_id, stars, Address) VALUES (36, N'Faena', 41, 5, N'Miami Beach');
 INSERT INTO hotel (id, name, city_id, stars, Address) VALUES (50, N'The Istanbul hotel', 10, 5, N'');
 INSERT INTO hotel (id, name, city_id, stars, Address) VALUES (51, N'hotel Dariush', 6, 5, N'');
 INSERT INTO hotel (id, name, city_id, stars, Address) VALUES (52, N'The Saint hotel', 201, 5, N'');
@@ -14,8 +15,15 @@ INSERT INTO hotel (id, name, city_id, stars, Address) VALUES (53, N'hotel Rialto
 INSERT INTO hotel (id, name, city_id, stars, Address) VALUES (54, N'Giessbach hotel', 90, 5, N'');
 INSERT INTO hotel (id, name, city_id, stars, Address) VALUES (55, N'Adventure Cabins', 153, 0, N'');
 INSERT INTO hotel (id, name, city_id, stars, Address) VALUES (56, N'Chalet Tré la Vieux', 101, 4, N'');
-INSERT INTO hotel (id, name, city_id, stars, Address) VALUES (57, N'Faena', 41, 5, N'Miami Beach');
 
+INSERT INTO hotel_photo (id, hotel_id, photo_url) VALUES (11, 36, N'36/aa8bb489-bc82-4a52-b4c5-eebe7afca4f0.jpg');
+INSERT INTO hotel_photo (id, hotel_id, photo_url) VALUES (12, 36, N'36/3798cc72-36e6-4fd0-a4b8-2251e062991c.jpg');
+INSERT INTO hotel_photo (id, hotel_id, photo_url) VALUES (13, 36, N'36/45332905-aed5-42c2-8ea8-92e110c7a307.jpg');
+INSERT INTO hotel_photo (id, hotel_id, photo_url) VALUES (14, 36, N'36/100f4eeb-a71f-43f0-be10-0c298834d52c.jpg');
+INSERT INTO hotel_photo (id, hotel_id, photo_url) VALUES (15, 36, N'36/1d88c2e4-b31e-4f2d-87f3-59a6511652ab.jpg');
+INSERT INTO hotel_photo (id, hotel_id, photo_url) VALUES (16, 36, N'36/90cd1dac-d5f4-4abe-a5ea-397c327fb345.jpg');
+INSERT INTO hotel_photo (id, hotel_id, photo_url) VALUES (17, 36, N'36/9c46a5c2-731a-4357-80de-f3fcf6c81d68.jpg');
+INSERT INTO hotel_photo (id, hotel_id, photo_url) VALUES (18, 36, N'36/f09053df-d19d-4e98-845e-8a9a66ad0dbb.jpg');
 INSERT INTO hotel_photo (id, hotel_id, photo_url) VALUES (28, 50, N'50/6b11e240-fb57-410c-ada0-46ce86ac4e41.jpg');
 INSERT INTO hotel_photo (id, hotel_id, photo_url) VALUES (29, 50, N'50/cbff077a-1512-41ee-ba9b-bd5d92e19fb1.jpg');
 INSERT INTO hotel_photo (id, hotel_id, photo_url) VALUES (30, 51, N'51/0695b32b-626f-4b99-a06f-6e469e1ad8de.jpg');
@@ -37,14 +45,6 @@ INSERT INTO hotel_photo (id, hotel_id, photo_url) VALUES (45, 55, N'55/f68ba108-
 INSERT INTO hotel_photo (id, hotel_id, photo_url) VALUES (46, 55, N'55/b4fe0a0c-1445-4597-8ac3-ed8442c6207e.jpg');
 INSERT INTO hotel_photo (id, hotel_id, photo_url) VALUES (47, 55, N'55/a5d7756d-ee95-4be1-bd8a-e8f7117b4df8.jpg');
 INSERT INTO hotel_photo (id, hotel_id, photo_url) VALUES (48, 55, N'55/ca3b5fd7-7eb9-4485-920e-2049796ada99.jpg');
-INSERT INTO hotel_photo (id, hotel_id, photo_url) VALUES (50, 57, N'36/aa8bb489-bc82-4a52-b4c5-eebe7afca4f0.jpg');
-INSERT INTO hotel_photo (id, hotel_id, photo_url) VALUES (51, 57, N'36/3798cc72-36e6-4fd0-a4b8-2251e062991c.jpg');
-INSERT INTO hotel_photo (id, hotel_id, photo_url) VALUES (52, 57, N'36/45332905-aed5-42c2-8ea8-92e110c7a307.jpg');
-INSERT INTO hotel_photo (id, hotel_id, photo_url) VALUES (53, 57, N'36/100f4eeb-a71f-43f0-be10-0c298834d52c.jpg');
-INSERT INTO hotel_photo (id, hotel_id, photo_url) VALUES (54, 57, N'36/1d88c2e4-b31e-4f2d-87f3-59a6511652ab.jpg');
-INSERT INTO hotel_photo (id, hotel_id, photo_url) VALUES (55, 57, N'36/90cd1dac-d5f4-4abe-a5ea-397c327fb345.jpg');
-INSERT INTO hotel_photo (id, hotel_id, photo_url) VALUES (56, 57, N'36/9c46a5c2-731a-4357-80de-f3fcf6c81d68.jpg');
-INSERT INTO hotel_photo (id, hotel_id, photo_url) VALUES (57, 57, N'36/f09053df-d19d-4e98-845e-8a9a66ad0dbb.jpg');
             ";
             migrationBuilder.Sql(sql);
         }
